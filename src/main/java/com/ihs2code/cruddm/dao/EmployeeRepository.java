cc9c8fb257497f151876f9dfb58fadbd7ad380cd
Admin i <ihsanmpm@gmail.com>
2021-07-25 19:15:20 +0530

    Final, Spring REST, sorting, pagination, asc, dsc

--- src/main/java/com/ihs2code/cruddm/dao/EmployeeRepository.java
+++ src/main/java/com/ihs2code/cruddm/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.ihs2code.cruddm.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ihs2code.cruddm.entity.Employee;
 
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// no need to write additional code for CRUD
